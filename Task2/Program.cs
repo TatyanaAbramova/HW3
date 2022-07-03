22e85d8841e64f499e8df0d4c3abc6d4d10c6ce6
Tatyana Abramova <tanusha-atv@mail.ru>
2022-07-03 21:53:27 +0500

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты точки А:");
+int ax = int.Parse(Console.ReadLine());
+int ay = int.Parse(Console.ReadLine());
+int az = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B:");
+int bx = int.Parse(Console.ReadLine());
+int by = int.Parse(Console.ReadLine());
+int bz = int.Parse(Console.ReadLine());
+
+void printGipotenuza (double ax, double ay, double az, double bx, double by, double bz)
+{
+    double dx = ax - bx;
+    double dy = ay - by;
+    double dz = az - bz;
+    double c = Math.Sqrt(dx * dx + dy * dy + dz * dz);
+    System.Console.WriteLine(c);
+}
+printGipotenuza (ax, ay, az, bx, by, bz);
